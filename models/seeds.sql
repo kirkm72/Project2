b17f8f41fc7862a53d123ffa37e191c91c7b99da
Air Dan <danshandle@gmail.com>
2019-11-07 21:46:08 -0600

    scoreboard added, some CSS, also the tables and SQL DB map

---
+++ models/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO teams (team_name) VALUES ('Austin Bats');
+INSERT INTO teams (team_name) VALUES ('Austin Bats');
+INSERT INTO teams (team_name) VALUES ('Austin Bats');
+INSERT INTO teams (team_name) VALUES ('Austin Bats');
+INSERT INTO teams (team_name) VALUES ('Austin Bats');
+
+
+INSERT INTO players (player_name, jersey_number, position, handness_bat, team_id) VALUES ('McKringleberry', '08', 'LF', 'RH', 1);
+INSERT INTO players (player_name, jersey_number, position, handness_bat, team_id) VALUES ('McKringleberry', '08', 'CF', 'RH', 1);
+INSERT INTO players (player_name, jersey_number, position, handness_bat, team_id) VALUES ('McKringleberry', '08', 'RF', 'LH', 1);
+INSERT INTO players (player_name, jersey_number, position, handness_bat, team_id) VALUES ('McKringleberry', '08', '1B', 'RH', 1);
+INSERT INTO players (player_name, jersey_number, position, handness_bat, team_id) VALUES ('McKringleberry', '08', '2B', 'SW', 1);
+INSERT INTO players (player_name, jersey_number, position, handness_bat, team_id) VALUES ('McKringleberry', '08', 'SS', 'LH', 1);
+INSERT INTO players (player_name, jersey_number, position, handness_bat, team_id) VALUES ('McKringleberry', '08', '3B', 'RH', 1);
+INSERT INTO players (player_name, jersey_number, position, handness_bat, team_id) VALUES ('McKringleberry', '08', 'C', 'RH', 1);
+INSERT INTO players (player_name, jersey_number, position, handness_bat, team_id) VALUES ('McKringleberry', '08', 'P', 'SW', 1);
+INSERT INTO players (player_name, jersey_number, position, handness_bat, team_id) VALUES ('McKringleberry', '08', 'DH', 'RH', 1);
+
+
