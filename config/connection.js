f08e74445730b336e0945631419a5c0d36f3ffaa
Kirk McGlamery <kirkm72@att.net>
2019-11-12 20:42:15 -0600

    fixed background issue and bound advance to new button 12Nov Kirk.

--- config/connection.js
+++ config/connection.js
@@ -1,37 +1,14 @@
-<<<<<<< HEAD
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "baseball_db"
-});
-
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
-module.exports = connection;
-=======
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
+  password: "",
   database: "baseballdb"
 });
 
-
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
     return;
@@ -40,4 +17,3 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
->>>>>>> 4257d1b9da7301de3bda93945113203a938cf8a9
