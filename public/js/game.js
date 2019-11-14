30a5674134bb70852a2113932a5acf94a927eae6
Jeremy <52435014+PlainOlFred@users.noreply.github.com>
2019-11-14 15:06:21 -0600

    Paperclip pof (#43)
    * mysql connection
    
    * elint setting
    
    * soft commit
    
    * removed console.log
    
    * added routes to get teams roster
    
    * inserted arrow functions for readbility
    
    * added table with player stat comments; loope through roster partials
    
    * deleted white space
    
    * table partial for player stats
    
    * add selectone orm method
    
    * update ReadMe
    
    * added id params to routes
    
    * updated models to include team_id
    
    * event listeners for ajax
    
    * handlebars files update to inculde roster tables
    
    * delete example models
    
    * moved batter in to same row as roster
    
    * added bat btn to roster
    
    * connection password
    
    * Mysql syntax fix: ending statement
    
    * comment out unused routes
    
    * comment out unused models
    
    * commented out unused logic
    
    * remove bullets from team list
    
    * moved scripts to main.hbs; rmoved test buttons; grid adjustment
    
    * created dataset for each player; worte data to #batter-player-card
    
    * add seeds for season_bat_stats
    
    * gave card id="battter-card" to add data attr player_id
    
    * added  data-player_id to .player-at-bat-btn
    
    * hides batter-card on click of hit-btn/out-btn
    
    * shows batter-card on click .player-at-bat-btn
    
    * added disable attribute to player-at-bat-btn default false;  on click btn is disabled and color white
    
    * batterBox selector
    
    * added jumbotron with start game buttom
    
    * fixed missing " on away platyer-at-bat-btn
    
    * declared scoring intervals for track score for mactch
    
    * declared first inning in match function; check for end game
    
    * deleted white space
    
    * opinionized script tag

--- public/js/game.js
+++ public/js/game.js
@@ -1,38 +1,80 @@
 
 
+
 function match(homeTeam, awayTeam, location, startMatch) {
+
   /*Post to matches in baseballdb  */
-  let home_team_id = 1
-  let away_team_id = 2
+  let 
+    home_team_id = 1,
+    away_team_id = 2,
+    inning = 1,
+    visScoreInning = 0,
+    homeScoreInning = 0;
   startInning(home_team_id, away_team_id);
 };
 
 function startInning(){
     
     let 
-        inning = 1,
         visScoreInningText = $(`#visScore${inning}`),
         homeScoreInningText = $(`#homeScore${inning}`),
-        batterbox = $('#batter-card')
-        visScoreInning = 0,
-        homeScoreInning = 0;
+        batterbox = $('#batter-card');
+
+    visScoreInning = 0;
+    homeScoreInning = 0;
 
     batterbox.hide();
 
 
-        $('#hit-btn').on("click", function(){
-            console.log("hello")
-        })
-    Top()
-    Bottom()
-        
-        
 
+    atBatsTop();
 
+    
     visScoreInningText.text(visScoreInningText);
     homeScoreInningText.text(homeScoreInningText);
 
     inning ++;
+    if(inning >9 ) {
+        endGame();
+    } else {
+        
+        startInning();
+    }
+}
+
+function atBatsTop(){ //promises?
+    let 
+        outs = 0,
+        hits = 0;
+
+     do{
+        $('#out-btn').on('click', function() {
+         outs ++;
+            console.log(outs)   
+        });
+
+        $('#hit-btn').on('click', function() {
+            hits ++;
+            console.log(hits)
+            if(hits >= 4) {
+                visScoreInning ++
+             }
+        });
+
+
+
+     } while (outs < 3)
+    
+        atBatsBottom();
+       
+    
+
+
+    
+}
+
+function atBatsBotton() {
+    console.log("home Team turn")
 }
 
 $(document).on("click", '#start-match',function(){
