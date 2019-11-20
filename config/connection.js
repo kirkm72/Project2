5b40609d5440f4401a0b7bcce165b57b1fdd540e
Jeremy <52435014+PlainOlFred@users.noreply.github.com>
2019-11-19 20:31:31 -0600

    Paperclip pof (#48)
    * mysql connection
    
    * elint setting
    
    * soft commit
    
    * removed console.log
    
    * added routes to get teams roster
    
    * inserted arrow functions for readbility
    
    * added table with player stat comments; loope through roster partials
    
    * deleted white space
    
    * table partial for player stats
    
    * add selectone orm method
    
    * update ReadMe
    
    * added id params to routes
    
    * updated models to include team_id
    
    * event listeners for ajax
    
    * handlebars files update to inculde roster tables
    
    * delete example models
    
    * moved batter in to same row as roster
    
    * added bat btn to roster
    
    * connection password
    
    * Mysql syntax fix: ending statement
    
    * comment out unused routes
    
    * comment out unused models
    
    * commented out unused logic
    
    * remove bullets from team list
    
    * moved scripts to main.hbs; rmoved test buttons; grid adjustment
    
    * created dataset for each player; worte data to #batter-player-card
    
    * add seeds for season_bat_stats
    
    * gave card id="battter-card" to add data attr player_id
    
    * added  data-player_id to .player-at-bat-btn
    
    * hides batter-card on click of hit-btn/out-btn
    
    * shows batter-card on click .player-at-bat-btn
    
    * added disable attribute to player-at-bat-btn default false;  on click btn is disabled and color white
    
    * batterBox selector
    
    * added jumbotron with start game buttom
    
    * fixed missing " on away platyer-at-bat-btn
    
    * declared scoring intervals for track score for mactch
    
    * declared first inning in match function; check for end game
    
    * deleted white space
    
    * opinionized script tag
    
    * update readME
    
    * switch hitn-btn and  out-btn to classes
    
    * passes team_id  into match to to start match
    
    * making teams avaiable to match function
    
    * syntax clean up
    
    * adde data-home-id
    
    * deleted duplicate game script tag; removed disable attribute on .player-at-bat-btn
    
    * created api routes match;removed results field from orm query; removed passing 'results' from controller and models
    
    * removed result field from matches table
    
    * removed  titles on cards
    
    * move script tag into game.js
    
    * team rosters into to scroll tables
    
    * style team roster scroll table
    
    * added table wrapper for scroll tables
    
    * replace script tags with BS template tags ; FOSS jquery cdn
    
    * moved team btn into dropdowns
    
    * hide dropdown on click; added 'at bats' column to roster table
    
    * suceesfull top and bottom first innings
    
    * successfull 1st inning; now promises do not resolve unitl after one innning;  game logic uses isTop to target home/away variables
    
    * two successfull innings; moved variable declarations before atbats function;
    
    * edited Batter card: add title for batting avg
    
    * formatted batter card
    
    * config dotev
    
    * Unsuccessful: added ajax request for gif
    
    * commented out index.js
    
    * add JAWSDB env option
    
    * heroku deployement

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql");
 require('dotenv').config();
+let connection;
 
-if(process.env.JAWSDB_URL) {
-  const connection =mysql.createConnection(process.env.JAWSDB_URL):
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  const connection = mysql.createConnection({
+  connection = mysql.createConnection({
+
   host: "localhost",
   port: 3306,
   user: "root",
@@ -15,6 +17,8 @@ if(process.env.JAWSDB_URL) {
 }
 
 ;
+
+};
 
 connection.connect(function (err) {
   if (err) {
